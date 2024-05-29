e7435653aa61b2b550c65e64a7f3626fc9294c6e
Komal <komal135296@gmail.com>
2024-05-29 22:24:31 +0530

    demo for geminichat

---
+++ app/arrow.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const arrow = () => {
+  return (
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    fill="none"
+    stroke="currentColor" // Set stroke color to white
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M5 12h14"></path>
+    <path d="M12 5l7 7-7 7"></path>
+  </svg>
+  )
+}
